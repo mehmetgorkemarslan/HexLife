2a3b5d9c3c735e529b0a101de270945d84f62f6b
MustafaKaanCetin <mustafakaancetin@proton.me>
2026-01-20 21:07:39 +0300

    Basic Gene/Genome implementation.

--- src/neat/genotype/Gene.h
+++ src/neat/genotype/Gene.h
@@ -1,1 +1,46 @@
+// Separated from Genome.h for better modularity
+
 #pragma once
+
+namespace neat {
+
+    enum class NodeType {
+        INPUT,
+        HIDDEN,
+        OUTPUT,
+        BIAS
+
+    };
+
+    struct NodeGene {
+        int id = -1;
+        NodeType type = NodeType::HIDDEN;
+
+        // Constructors
+        NodeGene() = default;
+        NodeGene(int id_, NodeType type_) : id(id_), type(type_) {}
+
+        // Comparison operators based on id. C++20 automatically provides !=.
+        bool operator==(NodeGene const& o) const noexcept { return id == o.id; }
+        bool operator<(NodeGene const& o) const noexcept { return id < o.id; }
+    };
+
+    struct ConnectionGene {
+        int in_id = -1;
+        int out_id = -1;
+        int innovation_number = -1;
+        double weight = 0.0;
+        bool enabled = true;
+
+        // Constructors
+        ConnectionGene() = default;
+        ConnectionGene(int in_id_, int out_id_, int innovation_number_, double weight_, bool enabled_)
+            : in_id(in_id_), out_id(out_id_), innovation_number(innovation_number_), weight(weight_), enabled(enabled_) {}
+
+        // Comparison operators based on innovation_number.
+        bool operator==(ConnectionGene const& o) const noexcept { return innovation_number == o.innovation_number; }
+        bool operator!=(ConnectionGene const& o) const noexcept { return innovation_number != o.innovation_number; }
+        bool operator<(ConnectionGene const& o) const noexcept { return innovation_number < o.innovation_number; }
+        bool operator>(ConnectionGene const& o) const noexcept { return innovation_number > o.innovation_number; }
+    };
+} // namespace neat
--- src/neat/genotype/Genome.h
+++ src/neat/genotype/Genome.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <algorithm>
+#include <vector>
+#include "Gene.h"
+
+namespace neat {
+    struct Genome {
+        std::vector<NodeGene> nodes;
+        std::vector<ConnectionGene> connections;
+    };
+
+    inline void sort_genes(Genome &genome) {
+        std::sort(genome.nodes.begin(), genome.nodes.end());
+        std::sort(genome.connections.begin(), genome.connections.end());
+    }
+
+    // Find connection using innovation_number.
+    inline int find_connection(Genome &genome, int innovation) {
+        for (size_t i = 0; i < genome.connections.size(); ++i) {
+            if (genome.connections[i].innovation_number == innovation) {
+                return static_cast<int>(i);
+            }
+        }
+        return -1;
+    }
+} // namespace neat
